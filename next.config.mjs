bf8f2a52f321a66513c8c51436dba5523ff47ba7
DeividFrancis <deividfrancisdeoliveira@gmail.com>
2024-03-04 21:35:45 -0400

    feat: config minima para converter para pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true
+  }
+});
+
+export default withPWA({
+  
+  // Your Next.js config
+});
